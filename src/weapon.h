0c336c2ada98b2593ae65d4f9cc47df608cb2f4b
honchokomodo <70309110+honchokomodo@users.noreply.github.com>
2025-08-03 11:50:26 -0700

    reorder thing in weapon.h

--- src/weapon.h
+++ src/weapon.h
@@ -16,20 +16,8 @@ typedef struct weapon {
 	Affix stat;
 } Weapon;
 
-Weapon skyward_harp_r1_90 = {
-/*
-Echoing Ballad
-Increases CRIT DMG by 20%. Hits have a 60% chance to inflict a small AoE
-attack, dealing 125% Physical ATK DMG. Can only occur once every 4s.
-*/
-	.type = SKYWARD_HARP,
-	.refinement = 1,
-	.atk = 674,
-	.stat = {CRIT_RATE, 22.1},
-};
-
 Weapon calamity_of_eshu_r5_90 = {
-/*
+/* from wiki:
 Diffusing Boundary
 While characters are protected by a Shield, DMG dealt by Normal and Charged
 Attacks is increased by 40%, and Normal and Charged Attack CRIT Rate is
@@ -39,6 +27,18 @@ increased by 16%.
 	.refinement = 5,
 	.atk = 565,
 	.stat = {ATK_PERCENT, 27.6},
+};
+
+Weapon skyward_harp_r1_90 = {
+/* from wiki:
+Echoing Ballad
+Increases CRIT DMG by 20%. Hits have a 60% chance to inflict a small AoE
+attack, dealing 125% Physical ATK DMG. Can only occur once every 4s.
+*/
+	.type = SKYWARD_HARP,
+	.refinement = 1,
+	.atk = 674,
+	.stat = {CRIT_RATE, 22.1},
 };
 
 #endif
